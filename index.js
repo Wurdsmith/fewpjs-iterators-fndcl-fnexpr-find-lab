4d7a2a5774de148910629aa721749edc410e6d29
Wurdsmith <wurdsmithing@gmail.com>
2021-04-25 20:33:29 -0500

    commit tree 0bfd04bd4e77c4b8520a6adcc6c3411c478d68dd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+] 
 
+function superbowlWin(array){
+    let record = array.find(s => {return s.result === "W"})
+    return !!record ? record.year : undefined
+}
